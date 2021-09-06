1103e49bb46d8822c489c0ba3ca28deece5bf519
ronentgreat <ronenh53@gmail.com>
2021-09-06 11:56:53 +0000

    debugging in progress I think ;)

--- GPA.py
+++ GPA.py
@@ -6,116 +6,122 @@ courses = []
 hours = []
 grades = []
 gradePoint = []
-numberOfCourses = int(input("Enter the number of courses you offers: \n"))
+
+try:
+    numberOfCourses = int(input("Enter the number of courses you offer: \n"))
 
 
+    # Created a function to get the courses the student offers
+    def course_select():
+        begin = 0
+        while begin != numberOfCourses:
+            ask = input("Please enter the course you offer: \n")
+            courses.append(ask.upper())
+            begin += 1
+            print(courses)
 
-# Created a function to get the courses the student offers
-def course_select():
-    begin = 0
-    while begin != numberOfCourses:
-        ask = input("Please enter the course you offer: \n")
-        courses.append(ask.upper())
-        begin += 1
+
+    course_select()
+    # Checks if the courses the student entered is correct
+
+    print("There these the courses you offer? \n")
+
+
+    def list_make():
+        for course in courses:
+            print(course)
+
+
+    list_make()
+    response = input("Y = Yes or N = No: \n")
+
+    if response == 'Y':
+        print("Very good! Let continue then.")
+        # Entering of credit hours
+        begin = 0
+        while begin != numberOfCourses:
+            print("Please enter the credit hours for the following courses: ")
+            list_make()
+            time = input(">>")
+            hours.append(int(time))
+            begin += 1
+            print(hours)
+        print("Very Good. Let's move on.")
+        print("These are the courses and their respective credit hours.")
+        for course, hour in zip(courses, hours):
+            print(course, ": ", hour)
+
+        total_credit = sum(hours)
+
+        # Entering of Grades
+        print("Please enter the grades you had in the following courses respectively.")
         print(courses)
 
-course_select()
-# Checks if the courses the student entered is correct
+        begin = 0
+        while begin != numberOfCourses:
+            ask_grade = input(">>")
+            grades.append(ask_grade.upper())
+            begin += 1
+        print(grades)
 
-print("There these the courses you offer? \n")
+        # Determination of grade points based on grades(A, B, C, D,...) entered
+        for hour, grade in zip(hours, grades):
+            if grade == "A":
+                gpt = hour * 4.0
+                gradePoint.append(gpt)
+            elif grade == "B+":
+                gpt = hour * 3.5
+                gradePoint.append(gpt)
+            elif grade == "B":
+                gpt = hour * 3.0
+                gradePoint.append(gpt)
+            elif grade == "C+":
+                gpt = hour * 2.5
+                gradePoint.append(gpt)
+            elif grade == "C":
+                gpt = hour * 2.0
+                gradePoint.append(gpt)
+            elif grade == "D+":
+                gpt = hour * 1.5
+                gradePoint.append(gpt)
+            elif grade == "D":
+                gpt = hour * 1.0
+                gradePoint.append(gpt)
+            elif grade == "E":
+                gpt = hour * 0.5
+                gradePoint.append(gpt)
+            elif grade == "F":
+                gpt = hour * 0.0
+                gradePoint.append(gpt)
+            else:
+                print("Something went wrong :(")
 
+        totalGradePoint = sum(gradePoint)
+        gpa = totalGradePoint / total_credit
+        print("Your GPA is ", round(gpa, 20))
 
-def list_make():
-    for course in courses:
-        print(course)
+        # Break down of grading
 
+        print("Would like to view the break of your course? \nY=Yes N=No \n")
+        answer = input(">>")
 
-list_make()
-response = input("Y or N: \n")
+        if answer.upper() == "Y":
+            print("Course ", " ", "Credit", " ", "Grade", " ", "Grade point")
+            for course, hour, grade, point in zip(courses, hours, grades, gradePoint):
+                print(course, "\t\t", hour, "\t\t", grade, "\t\t", point)
+            # Print total credit hours, total grade point here
+            print("Total credit hours is ", total_credit, " hours.")
+            print("Total grade point is ", totalGradePoint, ".")
+        else:
+            print("Good bye then ;).")
 
-if response == 'Y':
-    print("Very good! Let continue then.")
-elif response == 'N':
-    print("Seems you have made a mistake with your courses, kindly re-enter your courses again: \n")
-    course_select()
-else:
-    print("Please enter Y or N.")
-
-# Entering of credit hours
-begin = 0
-while begin != numberOfCourses:
-    print("Please enter the credit hours for the following courses: ")
-    list_make()
-    time = input(">>")
-    hours.append(int(time))
-    begin += 1
-    print(hours)
-print("Very Good. Let's move on.")
-print("These are the courses and their respective credit hours.")
-for course, hour in zip(courses, hours):
-    print(course, ": ", hour)
-
-total_credit = sum(hours)
-
-# Entering of Grades
-print("Please enter the grades you had in the following courses respectively.")
-print(courses)
-
-begin = 0
-while begin != numberOfCourses:
-    ask_grade = input(">>")
-    grades.append(ask_grade.upper())
-    begin += 1
-print(grades)
-
-
-# Determination of grade points based on grades(A, B, C, D,...) entered
-for hour, grade in zip(hours, grades):
-    if grade == "A":
-        gpt = hour * 4.0
-        gradePoint.append(gpt)
-    elif grade == "B+":
-        gpt = hour * 3.5
-        gradePoint.append(gpt)
-    elif grade == "B":
-        gpt = hour * 3.0
-        gradePoint.append(gpt)
-    elif grade == "C+":
-        gpt = hour * 2.5
-        gradePoint.append(gpt)
-    elif grade == "C":
-        gpt = hour * 2.0
-        gradePoint.append(gpt)
-    elif grade == "D+":
-        gpt = hour * 1.5
-        gradePoint.append(gpt)
-    elif grade == "D":
-        gpt = hour * 1.0
-        gradePoint.append(gpt)
-    elif grade == "E":
-        gpt = hour * 0.5
-        gradePoint.append(gpt)
-    elif grade == "F":
-        gpt = hour * 0.0
-        gradePoint.append(gpt)
+    elif response == 'N':
+        print("Seems you have made a mistake with your courses, kindly re-enter your courses again: \n")
+        course_select()
     else:
-        print("Something went wrong :(")
+        print("Please enter Y or N.")
 
-totalGradePoint = sum(gradePoint)
-gpa = totalGradePoint / total_credit
-print("Your GPA is ", round(gpa, 20))
 
-#Break down of grading
+except (ValueError, NameError):
+    print("You have entered an invalid input.")
 
-print("Would like to view the break of your course? \nY=Yes N=No \n")
-answer = input(">>")
-
-if answer.upper() == "Y":
-    print("Course ", " ", "Credit", " ", "Grade", " ", "Grade point")
-    for course, hour, grade, point in zip(courses, hours, grades, gradePoint):
-        print(course, "\t\t", hour, "\t\t", grade, "\t\t", point)
-# Print total credit hours, total grade point here
-    print("Total credit hours is ", total_credit, " hours.")
-    print("Total grade point is ", totalGradePoint, ".")
-else:
-    print("Good bye then ;).")
